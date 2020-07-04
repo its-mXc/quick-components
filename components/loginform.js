d735e583f2a066953c7d65227d4b2fb6a898421b
Uday Nayak <udaynayakgkv@gmail.com>
2020-07-04 21:00:38 +0530

    Login form componenet improved

--- components/loginform.js
+++ components/loginform.js
@@ -1,27 +1,25 @@
 import React,  {useState} from 'react';
 import {Form, Button} from 'react-bootstrap'
 
-const LoginForm = ({handleEmailChange, handlePasswordChange, handleLogin}) => {
+const LoginForm = ({emailLabel, emailPlaceholder, passwordLabel, passwordPlaceholder, buttonText, handleEmailChange, handlePasswordChange, handleLogin}) => {
   
   return (
     <Form onSubmit={handleLogin} className="w-25 m-3">
       <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} />
-        <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-        </Form.Text>
+        <Form.Label>{emailLabel || "Email address"}</Form.Label>
+        <Form.Control type="email" placeholder={emailPlaceholder || "Enter email"} onChange={handleEmailChange} />
       </Form.Group>
 
       <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange} />
+        <Form.Label>{passwordLabel || "Password"}</Form.Label>
+        <Form.Control type="password" placeholder={passwordPlaceholder || "Password"} onChange={handlePasswordChange} />
       </Form.Group>
+
       <Form.Group controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
       <Button variant="primary" type="submit">
-        Login
+        {buttonText || "Login"}
       </Button>
     </Form>
   )
